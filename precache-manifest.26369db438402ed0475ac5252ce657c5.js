4283dead186ffa46eef0d33b2fd3f49e4b4a59b7
Anurag Mathews <anuragmathews619@gmail.com>
2019-06-15 02:23:25 +0530

    Updates

---
+++ precache-manifest.26369db438402ed0475ac5252ce657c5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee1fd76bbf9f7fac89eb4244a0347a91",
+    "url": "/react-countdown/index.html"
+  },
+  {
+    "revision": "12ff700718d3f56a1533",
+    "url": "/react-countdown/static/css/main.f22f35ec.chunk.css"
+  },
+  {
+    "revision": "efc3d230fd1e1b3caa34",
+    "url": "/react-countdown/static/js/2.bbcd3f81.chunk.js"
+  },
+  {
+    "revision": "12ff700718d3f56a1533",
+    "url": "/react-countdown/static/js/main.91db3d10.chunk.js"
+  },
+  {
+    "revision": "00116c13f419d131f914",
+    "url": "/react-countdown/static/js/runtime~main.19a56f60.js"
+  },
+  {
+    "revision": "0a126f01c2c78026b490d52051315c08",
+    "url": "/react-countdown/static/media/ICC-Cricket-World-Cup-2019-Logo-PNG.0a126f01.png"
+  },
+  {
+    "revision": "d6c62e96916c12cd1291e6b08f9034d8",
+    "url": "/react-countdown/static/media/flag-india.d6c62e96.png"
+  },
+  {
+    "revision": "c865925d5cdc0531b1127291a6d9758e",
+    "url": "/react-countdown/static/media/flag-pak.c865925d.png"
+  }
+]);
